604d090c0f5ab84eb3662e05009ec80c1f695d46
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-03-31 14:30:27 +0530

    Improve security by also checking for `.dmg` file

--- package_downloader/main.py
+++ package_downloader/main.py
@@ -9,14 +9,16 @@ def run() -> None:
     url: str = input("Enter a url of Resource: ")
     name: str = input("Enter a name of Resource: ")
 
-    if name.endswith(".exe"):
-        print("Are you sure you want download a exe from the url...")
+    if name.endswith(".exe") or name.endswith(".dmg"):
+        print("Are you sure you want download a executable from the url?...")
         print("Please check for any virus, \
-              they might or might not be there in exe...")
+              they might or might not be there in executable...")
         if input("press y to continue downloading: ").lower() != "y":
             print("Thanks for using Package Downloader!!!!")
             return
 
+    print()
     result = downloader.download(url, name)
 
     print("Resource is saved successfully!!: " + result)
+    print("Thanks for using Package Downloader!!!!")
