613ce04c562b3dfb49560c7fb3b9054be3c33047
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-03-31 13:49:44 +0530

    make a command line app for downloading stuff

--- package_downloader/main.py
+++ package_downloader/main.py
@@ -1,2 +1,20 @@
+from .app import downloader
+
+
 def run():
-    pass
+    print("Welcome to Package Downloader\n")
+
+    url: str = input("Enter a url of Resource: ")
+    name: str = input("Enter a name of Resource: ")
+
+    if name.endswith(".exe"):
+        print("Are you sure you want download a exe from the url...")
+        print("Please check for any virus, \
+              they might or might not be there in exe...")
+        if input("press y to continue downloading: ").lower() != "y":
+            print("Thanks for using Package Downloader!!!!")
+            return
+
+    result = downloader.download(url, name)
+
+    print("Resource is saved successfully!!: " + result)
